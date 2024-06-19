4fd7f11b5a10929d7a8ee95203348dc0e72cf148
Enrique Izel <eleaoizel@gmail.com>
2024-06-19 15:44:06 -0400

    feat: add shared default configuration

--- app/src/main/java/com/example/sharedprefs/MainActivity.kt
+++ app/src/main/java/com/example/sharedprefs/MainActivity.kt
@@ -1,26 +1,41 @@
 package com.example.sharedprefs
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
+
+  lateinit var tvNotification: TextView
 
   companion object {
     const val NAME = "shared_prefs_file"
     const val KEY_NOTIFICATION = "notification_key"
   }
 
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    setupSharedPref()
+    tvNotification = findViewById(R.id.tv_notification)
+
+    setupDefaultSharedPrefs()
   }
+
+  private fun setupDefaultSharedPrefs() {
+    val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this)
+    val notificationTag = sharedPrefs.getBoolean(KEY_NOTIFICATION, false)
+
+    tvNotification.text = "Notification $notificationTag"
+
+    sharedPrefs.registerOnSharedPreferenceChangeListener(this)
+  }
+
 
   private fun setupSharedPref() {
     val sharedPrefs = getSharedPreferences(NAME, Context.MODE_PRIVATE)
@@ -36,5 +51,18 @@ class MainActivity : AppCompatActivity() {
 
     val notificationTrue = sharedPrefs.getBoolean(KEY_NOTIFICATION, false)
     Log.i("mainShared", "setupSharedPref: $notificationTrue")
+  }
+
+  override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+    if (key == KEY_NOTIFICATION) {
+      val notification = sharedPreferences?.getBoolean(key, false)
+      tvNotification.text = "Notification $notification"
+    }
+  }
+
+  override fun onDestroy() {
+    val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this)
+    sharedPrefs.unregisterOnSharedPreferenceChangeListener(this)
+    super.onDestroy()
   }
 }
