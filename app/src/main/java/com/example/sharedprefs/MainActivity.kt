8824e19c5f9e5543aa7b94468a1ddad11e3bbf1e
Enrique Izel <eleaoizel@gmail.com>
2024-06-19 10:14:16 -0400

    feat: add shared configuration

--- app/src/main/java/com/example/sharedprefs/MainActivity.kt
+++ app/src/main/java/com/example/sharedprefs/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.sharedprefs
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+  companion object {
+    const val NAME = "shared_prefs_file"
+    const val KEY_NOTIFICATION = "notification_key"
+  }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    enableEdgeToEdge()
     setContentView(R.layout.activity_main)
-    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-      insets
-    }
+
+    setupSharedPref()
+  }
+
+  private fun setupSharedPref() {
+    val sharedPrefs = getSharedPreferences(NAME, Context.MODE_PRIVATE)
+    val notification = sharedPrefs.getBoolean(
+      KEY_NOTIFICATION,
+      false
+    )
+    Log.i("mainShared", "setupSharedPref: $notification")
+
+    val editor = sharedPrefs.edit()
+    editor.putBoolean(KEY_NOTIFICATION, true)
+    editor.apply()
+
+    val notificationTrue = sharedPrefs.getBoolean(KEY_NOTIFICATION, false)
+    Log.i("mainShared", "setupSharedPref: $notificationTrue")
   }
 }
